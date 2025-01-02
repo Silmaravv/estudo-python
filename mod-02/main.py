e9f00771b09305a9b2f8f25c2f8e75c30bec2370
Silmara Vasconcelos Vitorino <vsilmaravit@gmail.com>
2025-01-02 20:13:42 -0300

    estudo type

---
+++ mod-02/main.py
@@ -0,0 +1,14 @@
+#print("Jesus é o caminho"[12])
+
+#print(10+10)
+num_char = len(input('qual seu nome? '))
+new_num_char = str(num_char)
+
+print ('seu nome tem' + new_num_char + ' caracteres')
+
+#adivinhe o que será impresso sem executar
+
+print(70 + float ('100.5'))
+
+#num_char = len(input('Qual o seu nome '))
+
---
+++ mod-02/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print(type(num_char))
