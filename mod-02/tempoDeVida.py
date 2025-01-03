49cbf283f6ccd29b2675be0dfcd1dccf18e16ff2
Silmara Vasconcelos Vitorino <vsilmaravit@gmail.com>
2025-01-03 19:49:23 -0300

    atualizando
    atividades TipsCode e 1º projeto solicitado no curso de Python
    desenvolver uma calculadora de gorjetas.

---
+++ mod-02/tempoDeVida.py
@@ -0,0 +1,6 @@
+'''escreva um programa em python que irá calcular em média
+ quantas semanas de vida lhe resta'''
+idade = int(input('informe sua idade'))
+anos = 90 - idade
+semanas = anos * 52
+print(semanas)
