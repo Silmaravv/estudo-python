325c59b19f57345be31d82d4877eb4cbf8a8d70c
Silmara Vasconcelos Vitorino <vsilmaravit@gmail.com>
2025-01-12 20:41:50 -0300

    Adiciona código de loop para exibição de habilidades e mensagem de identificação
    Implementado (teste)  um loop for para iterar sobre uma lista de habilidades e imprimir cada uma no console.
    Atividade relacionada à instalação e teste do PyCharm.

---
+++ mod-05/loop.py
@@ -0,0 +1,8 @@
+#Atividade de loop, intalação e teste PyCharm
+
+skill = ["Foco", "criatividade", "comunicação", "Gestão"]
+
+for skill in skill:
+    print(skill)
+
+print('Silmara Analista de Desenvolvimento de Sistemas')
